3b1842c759d9ed31f65361a7af2050a66b3be94c
Sofia Perone <sofiaperone62@gmail.com>
2021-03-13 03:45:20 -0300

    cambio1

---
+++ src/components/nav-bar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const NavBar = () => { return ( 
+
+ <div>  
+    
+   
+      <div class="header__menu">
+      <div> <h1>Gea Aromas! </h1></div>
+
+             <div class=" btn-group " role="group" aria-label="Button group with nested dropdown">
+    
+                 <button type="button" class="btn btn-primary  btn-lg">Home</button>
+                 <a class="btn btn-secondary btn-lg" href="../coder2/secciones/comoComprar.html" role="button">Como comprar</a>
+                 <a class="btn btn-secondary btn-lg" href="../coder2/secciones/envios.html" >Envios y Pagos</a>
+                 <a class="btn btn-secondary  btn-lg" href="../coder2/secciones/contactanos.html">Contactanos </a>
+                 <a class="btn btn-secondary btn-lg" href="../coder2/secciones/productos.html" role="button">Productos</a>
+     
+ </div>  </div> </div>  ); 
+
+};
