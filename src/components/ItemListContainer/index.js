13c7d9067762bd934bd62517badf115cc6d4019b
Sofia Perone <sofiaperone62@gmail.com>
2021-03-29 22:47:57 -0300

    desafio 6

--- src/components/ItemListContainer/index.js
+++ src/components/ItemListContainer/index.js
@@ -18,8 +18,9 @@ export default function ItemListContainer () {
               {id:1, nombre:"Velas", precio:200, pictureUrl:"https://http2.mlstatic.com/D_NQ_NP_859251-MLA26646040874_012018-O.webp" },
               {id:2, nombre:"Sahumerios", precio:150, pictureUrl:"http://d3ugyf2ht6aenh.cloudfront.net/stores/604/489/products/sahucanva1-cfacf168fcd896de0715221684219844-640-0.jpg" },
               {id:3, nombre:"Sales", precio:350, pictureUrl:"https://http2.mlstatic.com/sales-de-bano-D_NQ_NP_822421-MLM20754894508_062016-O.jpg" },
-              {id:4, nombre:"Bombitas", precio:350, pictureUrl:"https://th.bing.com/th/id/OIP.JuEZdzo9OFiu_JUNbwVPigHaFj?pid=ImgDet&rs=1" },
-              {id:5, nombre:"Carbones", precio:350, pictureUrl:"https://www.eldurapi.be/cms/CustomMedia/photos/Pages/es-eldurapi-AROMATICO-CARBON-DE-COCO_389_2.jpg" }
+              {id:4, nombre:"Bombitas", precio:300, pictureUrl:"https://th.bing.com/th/id/OIP.JuEZdzo9OFiu_JUNbwVPigHaFj?pid=ImgDet&rs=1" },
+              {id:5, nombre:"Carbones", precio:280, pictureUrl:"https://www.eldurapi.be/cms/CustomMedia/photos/Pages/es-eldurapi-AROMATICO-CARBON-DE-COCO_389_2.jpg" },
+              {id:5, nombre:"Recipientes", precio:275, pictureUrl:"https://th.bing.com/th/id/OIP.e1Oo-H8HPVjzu1A4y0mq8AHaHN?pid=ImgDet&w=600&h=584&rs=1" }
               
              
             
